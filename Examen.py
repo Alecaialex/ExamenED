f4ee4a5fcb3b037332aa258b3e1bb80546a17a88
alex <alecaialex@gmail.com>
2025-03-12 19:24:44 +0100

    buscarPalabra

--- Examen.py
+++ Examen.py
@@ -1,10 +1,13 @@
 def buscarPalabra(objetivo, palabras):
-
+    if objetivo in palabras: return True
+    else: return False
 
 
 def imprimirListaInversa(lista):
     for item in range(len(lista) -1, -1, -1):
-        print(lista[item])
+        print(f"- {lista[item]}")
+        
+    print()
 
 nombres = ["Mengano", "Fulano", "Zutano", "Perantano"]
 edades = {
@@ -14,4 +17,8 @@ edades = {
     "Perantano": 75
 }
 
-imprimirListaInversa(nombres)
+while True:
+    imprimirListaInversa(nombres)
+    nombre = input("\nBuscar nombre: ")
+    if buscarPalabra(nombre, nombres): print("Existe\n")
+    else: print("No existe\n")
