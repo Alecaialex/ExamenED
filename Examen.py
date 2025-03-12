2b1e5297929af1e92706fbf707534269d8fa09b8
alex <alecaialex@gmail.com>
2025-03-12 19:18:52 +0100

    imprimir inverso

---
+++ Examen.py
@@ -0,0 +1,17 @@
+def buscarPalabra(objetivo, palabras):
+
+
+
+def imprimirListaInversa(lista):
+    for item in range(len(lista) -1, -1, -1):
+        print(lista[item])
+
+nombres = ["Mengano", "Fulano", "Zutano", "Perantano"]
+edades = {
+    "Mengano": 0,
+    "Fulano": 25,
+    "Zutano": 50,
+    "Perantano": 75
+}
+
+imprimirListaInversa(nombres)
